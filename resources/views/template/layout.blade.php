abf98f889895d0da6fcfb3be2f3c35b9ddf8e21f
prandika119 <an.prasetya9@gmail.com>
2025-01-25 08:11:14 +0700

    update vercel route image, header security

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Andika Portfolio</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">'
     @vite('resources/css/app.css')
 </head>
 <body class="">
